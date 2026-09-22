68d3f3bd7ffb6cd3e18353214a040b9a6e86c8d2
idoit_melon <948794harvey@gmail.com>
2026-09-22 12:06:43 +0800

    fix pref submodule

---
+++ prefs/PrefsController.m
@@ -0,0 +1,12 @@
+#import "PrefsController.h"
+
+@implementation PrefsController
+
+- (NSArray *)specifiers {
+    if (!_specifiers) {
+        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+    }
+    return _specifiers;
+}
+
+@end
